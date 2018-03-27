efd134642c6846216753d5d78490fbd1a8c053d3
Anton Viktorov <anton.viktorov@returnonintelligence.com>
2018-03-27 20:28:27 +0300

    optimizations

--- src/main.cpp
+++ src/main.cpp
@@ -2,6 +2,8 @@
 #include <Arduino.h>
 #include <pins_arduino.h>
 
+#define __no_operation() {__asm__ __volatile__("nop");}
+
 #define PWR_ON_PIN PB0 // PB0
 #define ENA_IN_PIN PB1 // PB1
 
@@ -12,27 +14,21 @@
 
 unsigned long now = 0;
 unsigned long last_on = 0;
-unsigned char state = 0;
-unsigned char ena_in = 0;
 
 void setup() {
-  //TODO: make vice versa - all unused pins shall be INPUT, pulled down! 
-  PORTB = (1 << PWR_ON_PIN);
-  DDRB = 0xFF & ~(1 << ENA_IN_PIN);
+  DDRB = 0xFF & ~(1 << ENA_IN_PIN); // all pins out, excluding ENA_IN_PIN
+  PORTB = 0xFF & ~(1 << ENA_IN_PIN); // all pins up, including PWR_ON_PIN, excluding ENA_IN_PIN - its in tri state
+  __no_operation(); // sync
 }
 
 void loop() {
   now = millis();
-  ena_in = PINB & (1 << ENA_IN_PIN);
-
-  if (ena_in) {
+  if (PINB & (1 << ENA_IN_PIN)) {
     last_on = now;
-    state = ena_in;
     PORTB &= ~(1 << PWR_ON_PIN); // active low
   } else if (now >= last_on + HOLD_PERIOD) {
     last_on = 0;
-    state = 0;
     PORTB |= (1 << PWR_ON_PIN);
   }
-  __asm__ __volatile__("nop");
+  __no_operation(); //sync
 }
