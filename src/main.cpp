e585d53654af7496e887f17b21f959c78da486f0
Anton Viktorov <anton.viktorov@returnonintelligence.com>
2018-03-27 21:14:13 +0300

    Program: 270 bytes; Data: 12 bytes

--- src/main.cpp
+++ src/main.cpp
@@ -1,34 +1,33 @@
+#include <avr/io.h>
+#include "millis.h"
 
-#include <Arduino.h>
-#include <pins_arduino.h>
+#define nop() {__asm__ __volatile__("nop");}
 
-#define __no_operation() {__asm__ __volatile__("nop");}
+#define PWR_ON_PIN PB0
+#define ENA_IN_PIN PB1
 
-#define PWR_ON_PIN PB0 // PB0
-#define ENA_IN_PIN PB1 // PB1
+#define HOLD_PERIOD 1500 // ms
 
-#define HOLD_PERIOD 500
-// depends on real board and fuses. set up via experiment
-// on attiny2313 i used for test 1 = 8ms, so 200=1.6s
-// didnt test it on attiny13 yet
+uint32_t now = 0;
+uint32_t last_on = 0;
 
-unsigned long now = 0;
-unsigned long last_on = 0;
+int main(void) {
+  millis_init();
 
-void setup() {
-  DDRB = 0xFF & ~(1 << ENA_IN_PIN); // all pins out, excluding ENA_IN_PIN
-  PORTB = 0xFF & ~(1 << ENA_IN_PIN); // all pins up, including PWR_ON_PIN, excluding ENA_IN_PIN - its in tri state
-  __no_operation(); // sync
-}
+  DDRB = 0xFF & ~(1 << ENA_IN_PIN);  // all pins out, excluding ENA_IN_PIN
+  PORTB = 0xFF & ~(1 << ENA_IN_PIN); // all pins up, including PWR_ON_PIN - its low when active;
+                                     //              excluding ENA_IN_PIN - its in tri state
+  nop();
 
-void loop() {
-  now = millis();
-  if (PINB & (1 << ENA_IN_PIN)) {
-    last_on = now;
-    PORTB &= ~(1 << PWR_ON_PIN); // active low
-  } else if (now >= last_on + HOLD_PERIOD) {
-    last_on = 0;
-    PORTB |= (1 << PWR_ON_PIN);
+  while(1) {
+    now = millis();
+    if (PINB & (1 << ENA_IN_PIN)) {
+      last_on = now;
+      PORTB &= ~(1 << PWR_ON_PIN); // active low
+    } else if (now >= last_on + HOLD_PERIOD) {
+      last_on = 0;
+      PORTB |= (1 << PWR_ON_PIN);
+    }
+    nop();
   }
-  __no_operation(); //sync
 }
